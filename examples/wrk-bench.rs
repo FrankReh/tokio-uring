58bef552ef74b88ff203265361a4dfbf89877446
Frank Rehwinkel <frankrehwinkel@gmail.com>
2022-10-07 13:25:28 -0400

    Own changes I thought desirable to wrk-bench
    When first working with wrk-bench and rewrk,
    these changes came in handy.

--- examples/wrk-bench.rs
+++ examples/wrk-bench.rs
@@ -2,10 +2,10 @@ use std::io;
 use std::rc::Rc;
 use tokio::task::JoinHandle;
 
-pub const RESPONSE: &'static [u8] =
+const RESPONSE: &'static [u8] =
     b"HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
 
-pub const ADDRESS: &'static str = "127.0.0.1:8080";
+const ADDRESS: &'static str = "0.0.0.0:8090";
 
 fn main() -> io::Result<()> {
     tokio_uring::start(async {
@@ -21,10 +21,33 @@ fn main() -> io::Result<()> {
                     let (stream, _) = listener.accept().await?;
 
                     tokio_uring::spawn(async move {
-                        let (result, _) = stream.write(RESPONSE).await;
+                        let (result, _) = stream.write_all(RESPONSE).await;
 
                         if let Err(err) = result {
                             eprintln!("Client connection failed: {}", err);
+                            return;
+                        }
+
+                        // Continuous read from the TCP stream until the other side has closed it.
+                        let mut buf = vec![1u8; 256];
+                        loop {
+                            let (result, nbuf) = stream.read(buf).await;
+                            match result {
+                                Ok(0) => {
+                                    //println!("stream.read returned 0");
+                                    // Break when other side has closed.
+                                    break;
+                                }
+                                Ok(_n) => {
+                                    //println!("stream.read returned n: {}", _n);
+                                }
+                                Err(_err) => {
+                                    //println!("stream.read returned error: {}", _err);
+                                    // Break when other side has closed.
+                                    break;
+                                }
+                            }
+                            buf = nbuf;
                         }
                     });
                 }
